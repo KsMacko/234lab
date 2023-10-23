4e99c1712b1b1d84ff7e6e7da8ceba52dc775acc
Солнышко <Adeline1397@gmail.com>
2023-10-23 12:15:25 +0300

    2 лаба вроде работает

--- lab 2.py
+++ lab 2.py
@@ -4,6 +4,8 @@ def check_input(message):
         inp = int(input(message))
     except ValueError:
         print("Ошибка ввода! Пожалуйста, повторите!")
+    finally:
+        print("Без пнятия написать, но этот блок выполнится в любом случае\n")
     return inp
 
 
@@ -19,65 +21,67 @@ def task1(n):
 
 
 def task2(smth):
-    if type(smth, list):
+    if isinstance(smth, list):
         count = 0
         max_int = 0
         numbers = False
+        new_list = []
         for el in smth:
-            if type(el, int):
+            if isinstance(el, int):
                 numbers = True
                 if el % 2 == 0:
                     count += 1
                 if el > max_int:
                     max_int = el
-                if el < 0:
-                    smth.remove(el)
+                if el > 0:
+                    new_list.append(el)
         if numbers:
             print(f"Максимальное число - {max_int}")
             if count == 0:
                 print("Нет четных чисел!")
             else:
                 print(f"Количество четных чисел - {count}")
-            print(f"Новый список: {smth}")
+            print(f"Новый список: {new_list}")
         else:
             print("Список не содержит чисел!")
-    elif type(smth, dict):
+    elif isinstance(smth, dict):
         sorted_dict = sorted(smth.items(), key=lambda v: v[1], reverse=True)
         print(f"Отсортированный в обратном порядке по значениям словарь: {sorted_dict}\n")
-    elif type(smth, int):
+    elif isinstance(smth, int):
         rev_num = str(smth)
-        print(f"Перевернутое число - {rev_num[0:0:-1]}")
-    elif type(smth, str):
+        print(f"Перевернутое число - {rev_num[::-1]}\n")
+    elif isinstance(smth, str):
         d = {}
         for el in smth:
             if el not in d:
-                d[el] = f'{smth.count("el")}'
+                d[el] = f'{smth.count(el)}'
         print(d)
     else:
         print("Не подходящий тип данных!")
 
 
 def task3(m):
-    if type(m, list):
+    if isinstance(m, list):
         for i in m:
             print(f"{i}\n")
         rows = len(m)
         colums = len(m[0])
         true_col = 0
         summ = 0
-        negative = True
+
         for j in range(colums):
+            negative = True
             for i in range(rows):
                 if m[i][j] >= 0:
                     negative = False
                     break
-                if negative:
-                    true_col = j
+            if negative:
+                true_col = j
         if true_col >= 0:
             for j in range(colums):
                 for i in range(rows):
                     summ += m[i][j]
-            return summ/rows
+            return summ / rows
         else:
             print("Нет подхощях столбцов!")
     else:
@@ -87,7 +91,7 @@ def task3(m):
 def menu():
     choice = -1
     while choice != 0:
-        print("\nВыберите:\n1.Task 1\n2.Task 2\n3.Task3\n4.Task4")
+        print("\nВыберите:\n1.Task 1\n2.Task 2\n3.Task3")
         while choice < 0 or choice > 4:
             choice = check_input("\nВаш выбор - ")
         if choice == 1:
@@ -99,6 +103,19 @@ def menu():
             else:
                 number = task1(num)
             print(f"Число под номером {num} равно {number}")
+            choice = -1
+        elif choice == 2:
+            task2([1, 0, 1, -1, 22, -11, "d"])
+            print("\nПроверочный вариант")
+            task2(["h", "k", "d", "s"])
+            task2({"apples": 1, "grapes": 3, "pineapples": 2})
+            task2(1234)
+            task2("i love Python 8wgyyyy")
+            choice = -1
+        elif choice == 3:
+            print(f"Среднее арифметическое - {task3([[1, -3, 4], [-3, -4, 7], [0, -5, 1]])}")
+            print("Проверка")
+            print(f"Среднее арифметическое - {task3([[1, 2], [2, 1]])}")
             choice = -1
 
 
